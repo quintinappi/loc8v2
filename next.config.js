e4137656f5f49be38ba0ca7bc54e52f718bed5ab
Quintin <quintin@app-i.co.za>
2024-08-10 19:42:45 +0200

    Ready for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  fallbacks: {
+    document: '/offline.html'
+  }
 });
 
 /** @type {import('next').NextConfig} */
